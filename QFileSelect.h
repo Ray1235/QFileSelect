40f04e1074f4c351da35de8d4b74fb7e9905de2a
Maciej Zaremba <Ray1235@me.com>
2018-05-22 12:13:16 +0200

    Create QFileSelect.h

---
+++ QFileSelect.h
@@ -0,0 +1,28 @@
+#ifndef QFILESELECT_H
+#define QFILESELECT_H
+
+#include <QWidget>
+#include <QtWidgets>
+#include <QtGui>
+#include <QtCore>
+
+class QFileSelect : public QWidget
+{
+	Q_OBJECT
+
+public:
+	QFileSelect(QStringList fileFilter = QStringList(), QString label = "", QWidget *parent = NULL);
+	~QFileSelect();
+
+	QLabel *m_pPathLabel;
+	QHBoxLayout *m_pPathLayout;
+	QLineEdit *m_pPathBox;
+	QPushButton *m_pPathBrowse;
+
+	QFileDialog *m_pFileDialog;
+
+protected slots:
+	void OnBrowsePath();
+};
+
+#endif
